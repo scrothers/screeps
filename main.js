f7d9bfab8c88766a4b951a5a933d2ab6d468fd8b
Steven Crothers <steven@namecheap.com>
2017-02-24 19:48:02 -0500

    Blah

--- main.js
+++ main.js
@@ -1,48 +1,13 @@
-var HelperFunctions = require('HelperFunctions');
-var RoomHandler = require('RoomHandler');
-var ScoutHandler = require('ScoutHandler');
-var Room = require('Room');
-var CreepBase = require('CreepBase');
-var CreepScout = require('CreepScout');
+/*
+ * Load in all of the various modules we'll be using.
+ */
+var roleHarvester = require('roles/harvester');
 
+// Screeps Main CPU Loop
 module.exports.loop = function () {
 
-	//ScoutHandler.setRoomHandler(RoomHandler);
-
-	// Init rooms
-	for(var n in Game.rooms) {
-		var roomHandler = new Room(Game.rooms[n], RoomHandler);
-		RoomHandler.set(Game.rooms[n].name, roomHandler);
-	};
-
-	// Load rooms
-	var rooms = RoomHandler.getRoomHandlers();
-	for(var n in rooms) {
-		var room = rooms[n];
-		room.loadCreeps();
-		room.populate();
-
-		console.log(
-			room.room.name + ' | ' +
-			'goals met:' +
-			room.population.goalsMet() +
-			', population: ' +
-			room.population.getTotalPopulation() + '/' + room.population.getMaxPopulation() +
-			' (' + room.population.getType('CreepBuilder').total + '/' +
-			room.population.getType('CreepMiner').total + '/' +
-			room.population.getType('CreepCarrier').total + '/' +
-			room.population.getType('CreepSoldier').total +
-			'), ' +
-			'resources at: ' + parseInt( (room.depositManager.energy() / room.depositManager.energyCapacity())*100) +'%, ' +
-			'max resources: ' + room.depositManager.energyCapacity() +'u, ' +
-			'next death: ' + room.population.getNextExpectedDeath() +' ticks'
-		);
-	};
-
-	// Load scouts.
-	//ScoutHandler.loadScouts();
-	//ScoutHandler.spawnNewScouts();
-
-	HelperFunctions.garbageCollection();
-
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        roleHarvester.run(creep);
+    }
 }
