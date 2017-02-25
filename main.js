cf978b889e0ed9918102e49b0ad68d858370c413
Steven Crothers <steven@namecheap.com>
2017-02-24 20:10:21 -0500

    blanking

--- main.js
+++ main.js
@@ -6,22 +6,4 @@ var roleHarvester = require('roles.Harvester');
 // Screeps Main CPU Loop
 module.exports.loop = function () {
 
-  if(Game.spawns['Alpha'].spawning) {
-    var spawningCreep = Game.creeps[Game.spawns['Alpha'].spawning.name];
-    Game.spawns['Alpha'].room.visual.text(
-      '🛠️' + spawningCreep.memory.role,
-      Game.spawns['Alpha'].pos.x + 1,
-      Game.spawns['Alpha'].pos.y,
-      {align: 'left', opacity: 0.8});
-  }
-
-  for(var name in Game.creeps) {
-    var creep = Game.creeps[name];
-
-    if(creep.memory.role == 'harvester') {
-        roleHarvester.run(creep);
-    }
-  }
-
-  roleHarvester.spawn();
 }
