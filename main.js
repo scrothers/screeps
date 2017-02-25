dd8415fac856283bbd95ae532ecd514969cfabb8
Steven Crothers <steven@namecheap.com>
2017-02-24 19:49:34 -0500

    No folders bruh.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /*
  * Load in all of the various modules we'll be using.
  */
-var roleHarvester = require('roles/harvester');
+var roleHarvester = require('roles.Harvester');
 
 // Screeps Main CPU Loop
 module.exports.loop = function () {
