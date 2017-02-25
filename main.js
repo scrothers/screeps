e3daf495069b359854e12f602077fe21c571b916
Steven Crothers <steven@namecheap.com>
2017-02-24 20:07:42 -0500

    Blah

--- main.js
+++ main.js
@@ -6,8 +6,22 @@ var roleHarvester = require('roles.Harvester');
 // Screeps Main CPU Loop
 module.exports.loop = function () {
 
-    for(var name in Game.creeps) {
-        var creep = Game.creeps[name];
+  if(Game.spawns['Alpha'].spawning) {
+    var spawningCreep = Game.creeps[Game.spawns['Alpha'].spawning.name];
+    Game.spawns['Alpha'].room.visual.text(
+      '🛠️' + spawningCreep.memory.role,
+      Game.spawns['Alpha'].pos.x + 1,
+      Game.spawns['Alpha'].pos.y,
+      {align: 'left', opacity: 0.8});
+  }
+
+  for(var name in Game.creeps) {
+    var creep = Game.creeps[name];
+
+    if(creep.memory.role == 'harvester') {
         roleHarvester.run(creep);
     }
+  }
+
+  roleHarvester.spawn();
 }
--- roles.Harvester.js
+++ roles.Harvester.js
@@ -1,17 +1,32 @@
 module.exports = {
+  /**
+   * Create additional harversters according to specification.
+   */
+  spawn: function() {
+    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
 
-    /** @param {Creep} creep **/
-    run: function(creep) {
-        if(creep.carry.energy < creep.carryCapacity) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
-            }
-        }
-        else {
-            if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(Game.spawns['Spawn1']);
-            }
-        }
+    if(harvesters.length < 2) {
+      var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
+      console.log('Spawning new harvester: ' + newName);
     }
+  }
+
+  /**
+   * Control creeps and make them harvest.
+   *
+   * @param {Creep} creep
+   */
+  run: function(creep) {
+    if(creep.carry.energy < creep.carryCapacity) {
+      var sources = creep.room.find(FIND_SOURCES);
+      if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(sources[0]);
+      }
+    }
+    else {
+      if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(Game.spawns['Spawn1']);
+      }
+    }
+  }
 };
