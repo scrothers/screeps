27abd1915cc1eef71cd7428b68f0159b96f6577a
Steven Crothers <steven@namecheap.com>
2017-02-24 23:54:03 -0500

    Spawn name

--- main.js
+++ main.js
@@ -18,21 +18,21 @@ module.exports.loop = function () {
     var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
 
     if(harvesters.length < 2) {
-        var newName = Game.spawns['Alpha'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
+        var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
         console.log('Spawning new harvester: ' + newName);
     }
 
     if(harvesters.length < 1) {
-        var newName = Game.spawns['Alpha'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
+        var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
         console.log('Spawning new upgrader: ' + newName);
     }
 
-    if(Game.spawns['Alpha'].spawning) {
+    if(Game.spawns['Spawn1'].spawning) {
         var spawningCreep = Game.creeps[Game.spawns['Alpha'].spawning.name];
-        Game.spawns['Alpha'].room.visual.text(
+        Game.spawns['Spawn1'].room.visual.text(
             spawningCreep.memory.role,
-            Game.spawns['Alpha'].pos.x + 1,
-            Game.spawns['Alpha'].pos.y,
+            Game.spawns['Spawn1'].pos.x + 1,
+            Game.spawns['Spawn1'].pos.y,
             {align: 'left', opacity: 0.8});
     }
 
